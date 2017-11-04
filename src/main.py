2a58c68fc5f2d18f95f5d07d3017c105939f355a
Amir Eldor <amir.eldor@gmail.com>
2017-11-04 03:36:48 +0200

    Some re-organization in main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
-"""Pigeon Pictures - pictures of pigeons every X minutes"""
-
+"""
+Pigeon Pictures - https://pigeon.pictures
+  - a site with pictures of pigeons that change every X minutes
+"""
 import logging
 import settings
 from pigeons import URLFetcher, HTMLWriter
 
 
-def setupLogger():
-    """Pretty self-explanatory..."""
-    logging.basicConfig(filename=settings.LOG_FILENAME, level=settings.LOG_LEVEL)
-
-
 def main():
     """Main entry point of the program. I don't know what it will do.
-    Hopefully fetch pigeon pictures' URLs and spit them into an HTML"""
+    Hopefully fetch pigeon pictures' URLs and spit them into an HTML."""
 
-    setupLogger()
+    setup_logger()
+    logging.info("Hello from Pigeon Pictures!")
+    do_pigeon_pictures_magic()
+    logging.info("Goodbye.")
 
+
+def setup_logger():
+    """Pretty self-explanatory..."""
+    logging.basicConfig(filename=settings.LOG_FILENAME,
+                        level=settings.LOG_LEVEL)
+
+
+def do_pigeon_pictures_magic():
+    """By "magic", I mean doing what's this all about. Fetching pigeon URLs
+    and writing them nicely to an HTML."""
     fetcher = URLFetcher()
     pigeon_urls = fetcher.fetch_urls()
-
     writer = HTMLWriter()
     writer.write(settings.HTML_FILE, pigeon_urls)
 
