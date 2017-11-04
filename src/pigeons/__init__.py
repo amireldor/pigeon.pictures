26855db5d58861cb69aa62c558d4b64efd6da2cf
Amir Eldor <amir.eldor@gmail.com>
2017-11-05 01:20:24 +0200

    Making the search_term_generator nicer

--- src/pigeons/__init__.py
+++ src/pigeons/__init__.py
@@ -1,3 +1,4 @@
 """The pigeons module is the heart and soul of Pigeon Pictures"""
 from .url_fetcher import URLFetcher
 from .html_writer import HTMLWriter
+from .search_term_generator import generate as search_term_generator
---
+++ src/pigeons/search_term_generator.py
@@ -0,0 +1,29 @@
+"""Generates funny terms to search for, like "nice pigeon"."""
+from random import choice
+
+NOUNS = (
+    "pigeon",
+    "pigeons",
+    "domestic pigeon",
+    "homing pigeon",
+    "feral pigeon",
+)
+
+ADJECTIVES = (
+    "",
+    "nice",
+    "pretty",
+    "lovely",
+    "elegant",
+    "real",
+    "flying",
+    "eating",
+)
+
+def generate():
+    """Returns a random pigeon search term"""
+    pigeon_term = "{} {}".format(choice(ADJECTIVES), choice(NOUNS)).strip()
+    return pigeon_term
+
+if __name__ == "__main__":
+    print(generate())
