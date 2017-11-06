952099f116136f5faff1e40665fd0bf3122a86c2
Amir Eldor <amir@eize.ninja>
2017-11-07 01:35:24 +0200

    Playing around with tests

--- src/pigeons/url_fetcher.py
+++ src/pigeons/url_fetcher.py
@@ -1,5 +1,9 @@
 """This will fetch URLs of pigeons for the goodness of time"""
 
+class NoPigeonURLs(Exception):
+    pass
+
+
 class URLFetcher:
     """Fetch the damned URLs from somewhere"""
     def fetch_urls(self):
--- tests/url_fetcher_test.py
+++ tests/url_fetcher_test.py
@@ -1,7 +1,8 @@
 """Test that pigeon pictures' URLs are fetched nicely"""
-import pytest
-from pigeons import URLFetcher
+# pylint: disable=redefined-outer-name
 
+import pytest
+from pigeons import URLFetcher, NoPigeonURLs
 
 @pytest.fixture(scope="module")
 def fetcher():
@@ -9,8 +10,13 @@ def fetcher():
     return URLFetcher()
 
 
-def test_fetch_urls(fetcher):
+def test_fetch_urls_success(fetcher):
     """Test a successful request to the extenral API"""
     urls = fetcher.fetch_urls()
-    assert urls
+    assert type(urls) == type([])
 
+
+def test_fetch_urls_failure(fetcher):
+    """Test a successful request to the extenral API"""
+    with pytest.raises(NoPigeonURLs):
+        _ = fetcher.fetch_urls()
