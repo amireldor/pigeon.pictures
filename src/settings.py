74a92ee1e77c4bee8056bd34e3e3df5395504821
Amir Eldor <amir@eize.ninja>
2018-01-27 03:40:21 +0200

    Don't die on missing 'private.py', give warning
    I did this so I can run tests more easily. At least the current tests.
    The future may hold more complicated tests that might fail without some
    settings, at least default one.

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,9 @@
 """Various interesting Pigeon Pictures settings"""
 import logging
-from private import *  # pylint: disable=wildcard-import,unused-wildcard-import
+try:
+    from private import *  # pylint: disable=wildcard-import,unused-wildcard-import
+except ImportError:
+    logging.warn("No 'private' module! You might be doing some silly. See 'private.template.py'.")
 
 LOG_FILENAME = "log.pigeon.log"
 LOG_LEVEL = logging.INFO
