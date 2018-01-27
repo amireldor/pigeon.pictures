4fbff7a0c2cc022de930d2477b1ceca6afc03858
Amir Eldor <amir@eize.ninja>
2018-01-27 04:06:09 +0200

    An actual lambda that saves a proper index.html to S3

--- src/lambda.py
+++ src/lambda.py
@@ -5,14 +5,24 @@ Make the pigeon be fetched, save output to S3.
 
 import boto3
 import settings
+from main import fetch_urls, create_writer
 
 
 def main():
+    html = prepare_html()
+    write_to_s3(html)
+
+
+def prepare_html():
+    urls = fetch_urls()
+    writer = create_writer()
+    return writer.render(urls)
+
+
+def write_to_s3(html):
     s3 = boto3.resource("s3")
     write_to_me = s3.Object(settings.S3_BUCKET_NAME, settings.S3_OBJECT_KEY)
-
-    response = write_to_me.put(Body="This is a test")
-
+    response = write_to_me.put(Body=html)
     print(response)
 
 
