da501af513bd09f37121befc89ef7da175465e99
Amir Eldor <amir@eize.ninja>
2020-03-20 22:52:38 +0200

    WIP: Python 3.8, setup.py, start reorganizing things

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pigeonpictures",
+    version="1.1.0",
+    author="Amir Eldor",
+    author_email="amir@eize.ninja",
+    packages=find_packages(),
+    install_requires=["zappa", "jinja2"],
+)
