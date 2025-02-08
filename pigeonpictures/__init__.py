8c77e32da4c61528f8a9a8bc813894d162d4c286
Amir Eldor <amir@eldor.dev>
2025-02-08 18:31:41 +0200

    Delete everything

--- pigeonpictures/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This module is the heart and soul of Pigeon Pictures"""
-
-from .url_fetcher import GoogleCustomSearchFetcher, NoPigeonURLs
-from .html_writer import Jinja2HTMLWriter
-from .search_term_generator import generate_pigeon_search_term
