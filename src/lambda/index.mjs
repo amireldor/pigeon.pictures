2c77bbe006f2bdafe167182fd0ef2d0d64591142
Amir Eldor <amir@eldor.dev>
2025-09-23 23:11:39 +0300

    Managed to fetch pigeons and put them in S3

--- src/lambda/index.mjs
+++ src/lambda/index.mjs
@@ -1,26 +1,47 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const handler = async (event, context) => {
+  const PIGEONS_PER_PAGE = 20;
+  const page = Math.floor(Math.random() * 20) + 1;
+  const PIXABAY_API_KEY = process.env.PIXABAY_API_KEY;
+  const PIXABAY_API_URL = `https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=pigeon&image_type=photo&page=${page}&per_page=${PIGEONS_PER_PAGE}`;
+
+  const searchResponse = await fetch(PIXABAY_API_URL);
+  const searchData = await searchResponse.json();
+  const images = searchData.hits;
+
+  if (!images.length) {
+    throw new Error("No pigeon images found");
+  }
+
   const s3 = new S3Client({});
-
   const bucketName = process.env.BUCKET_NAME;
-  const objectKey = "test-object.txt";
-  const objectContent = "This is a test object created by the Lambda function.";
 
-  const putObjectCommand = new PutObjectCommand({
-    Bucket: bucketName,
-    Key: objectKey,
-    Body: objectContent,
+  const imageTasks = images.map(async (img, index) => {
+    const imageResponse = await fetch(img.largeImageURL);
+    const imageBuffer = await imageResponse.arrayBuffer();
+
+    const putObjectCommand = new PutObjectCommand({
+      Bucket: bucketName,
+      Key: `pigeons-2025/pigeons/${index.toString().padStart(2, "0")}.jpg`,
+      Body: Buffer.from(imageBuffer),
+      ContentType: "image/jpeg",
+    });
+
+    await s3.send(putObjectCommand);
   });
 
-  await s3.send(putObjectCommand);
+  const settled = await Promise.allSettled(imageTasks);
+  const failed = settled.filter((result) => result.status === "rejected");
 
-  console.log(
-    `Successfully created object ${objectKey} in bucket ${bucketName}`
-  );
+  if (failed.length > 0) {
+    console.error(`${failed.length} image uploads failed.`);
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify("Hello from pigeon.picture's AWS Lambda!"),
+    body: JSON.stringify(
+      `Yay! ${settled.length - failed.length} Pigeon Pictures deployed.`
+    ),
   };
 };
