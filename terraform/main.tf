c3d6f070eff3c68dbd48bdfe01b4ccd53a334d1e
Amir Eldor <amir@eldor.dev>
2025-09-24 12:31:26 +0300

    Cleanup Terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,33 @@
-# ACM certificate for pigeon.pictures (must be in us-east-1 for CloudFront)
+
+# -----------------------------------------------------------------------------
+# PROVIDERS
+# -----------------------------------------------------------------------------
+provider "aws" {
+  region = "eu-central-1"
+}
+
 provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
 }
 
+# -----------------------------------------------------------------------------
+# DATA SOURCES
+# -----------------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
+
+# -----------------------------------------------------------------------------
+# VARIABLES
+# -----------------------------------------------------------------------------
+variable "pixabay_api_key" {
+  type        = string
+  description = "The API key for Pixabay."
+  sensitive   = true
+}
+
+# -----------------------------------------------------------------------------
+# ACM CERTIFICATE (us-east-1, required for CloudFront custom domain)
+# -----------------------------------------------------------------------------
 resource "aws_acm_certificate" "pigeon_pictures" {
   provider          = aws.us_east_1
   domain_name       = "pigeon.pictures"
@@ -19,32 +43,13 @@ resource "aws_acm_certificate_validation" "pigeon_pictures" {
   validation_record_fqdns = [] # Fill with Route53 record FQDNs if automating DNS validation
 }
 
-# Output ACM certificate ARN for use in CloudFront
 output "pigeon_pictures_acm_arn" {
   value = aws_acm_certificate.pigeon_pictures.arn
 }
 
-provider "aws" {
-  region = "eu-central-1"
-}
-
-#########################
-# Data Source for Account ID
-#########################
-data "aws_caller_identity" "current" {}
-
-#########################
-# Variable for Pixabay API Key
-#########################
-variable "pixabay_api_key" {
-  type        = string
-  description = "The API key for Pixabay."
-  sensitive   = true
-}
-
-#########################
-# Secrets Manager for PIXABAY_API_KEY
-#########################
+# -----------------------------------------------------------------------------
+# SECRETS MANAGER (Pixabay API Key)
+# -----------------------------------------------------------------------------
 resource "aws_secretsmanager_secret" "pixabay_api_key" {
   name        = "pigeon-pictures-pixabay-api-key"
   description = "API key for Pixabay used by the pigeon.pictures website"
@@ -55,12 +60,13 @@ resource "aws_secretsmanager_secret_version" "pixabay_api_key_version" {
   secret_string = var.pixabay_api_key
 }
 
-# The nice bucket
+# -----------------------------------------------------------------------------
+# S3 BUCKET & WEBSITE CONFIGURATION
+# -----------------------------------------------------------------------------
 resource "aws_s3_bucket" "pigeon_pictures_bucket" {
   bucket = "pigeon.pictures"
 }
 
-# S3 static website configuration (recommended)
 resource "aws_s3_bucket_website_configuration" "pigeon_pictures_website" {
   bucket = aws_s3_bucket.pigeon_pictures_bucket.bucket
 
@@ -73,8 +79,6 @@ resource "aws_s3_bucket_website_configuration" "pigeon_pictures_website" {
   }
 }
 
-
-# Upload pixabay.svg asset to S3 at the desired key
 resource "aws_s3_object" "pixabay_svg" {
   bucket       = aws_s3_bucket.pigeon_pictures_bucket.id
   key          = "pigeons-2025/pixabay.svg"
@@ -83,20 +87,18 @@ resource "aws_s3_object" "pixabay_svg" {
   content_type = "image/svg+xml"
 }
 
-# A null resource to trigger the npm install command
+# -----------------------------------------------------------------------------
+# LAMBDA BUILD & DEPLOYMENT
+# -----------------------------------------------------------------------------
 resource "null_resource" "build_lambda" {
-  # The trigger ensures this resource is "recreated" whenever the package.json changes.
   triggers = {
     package_json_hash = filemd5("${path.module}/../src/lambda/package.json")
   }
-
-  # The local-exec provisioner runs a command on the machine executing Terraform.
   provisioner "local-exec" {
     command = "npm install --prefix ${path.module}/../src/lambda"
   }
 }
-#
-# Package the lambda function into a zip file
+
 data "archive_file" "lambda_zip" {
   depends_on  = [null_resource.build_lambda]
   type        = "zip"
@@ -104,7 +106,6 @@ data "archive_file" "lambda_zip" {
   output_path = "${path.module}/../tmp/lambda.zip"
 }
 
-# Lambda IAM role for S3 PutObject
 resource "aws_iam_role" "lambda_s3_put_role" {
   name = "pigeon-pictures-lambda-s3-put-role"
   assume_role_policy = jsonencode({
@@ -150,7 +151,6 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_put_policy_attach" {
   policy_arn = aws_iam_policy.lambda_s3_put_policy.arn
 }
 
-# Lambda function
 resource "aws_lambda_function" "pigeons_lambda" {
   function_name    = "pigeon-pictures-update-pigeons"
   role             = aws_iam_role.lambda_s3_put_role.arn
@@ -166,25 +166,23 @@ resource "aws_lambda_function" "pigeons_lambda" {
       PIXABAY_API_KEY = var.pixabay_api_key
     }
   }
-
-  # ...existing code...
 }
 
-# EventBridge rule to trigger Lambda every hour at 0 minutes
+# -----------------------------------------------------------------------------
+# EVENTBRIDGE SCHEDULE FOR LAMBDA
+# -----------------------------------------------------------------------------
 resource "aws_cloudwatch_event_rule" "pigeons_lambda_hourly" {
   name                = "pigeon-pictures-lambda-hourly"
   description         = "Triggers the pigeons Lambda every hour at 0 minutes"
   schedule_expression = "cron(0 * * * ? *)"
 }
 
-# EventBridge target to connect rule to Lambda
 resource "aws_cloudwatch_event_target" "pigeons_lambda_target" {
   rule      = aws_cloudwatch_event_rule.pigeons_lambda_hourly.name
   target_id = "PigeonsLambdaTarget"
   arn       = aws_lambda_function.pigeons_lambda.arn
 }
 
-# Allow EventBridge to invoke the Lambda
 resource "aws_lambda_permission" "allow_eventbridge_invoke" {
   statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
@@ -193,199 +191,9 @@ resource "aws_lambda_permission" "allow_eventbridge_invoke" {
   source_arn    = aws_cloudwatch_event_rule.pigeons_lambda_hourly.arn
 }
 
-
-
-#########################
-# IAM Role for CodeBuild (pigeon.pictures)
-#########################
-resource "aws_iam_role" "pigeon_codebuild_role" {
-  name = "pigeon-pictures-codebuild-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
-        Principal = { Service = "codebuild.amazonaws.com" }
-      }
-    ]
-  })
-}
-
-# Custom policy allowing S3 access only to the pigeon.pictures bucket.
-resource "aws_iam_policy" "pigeon_codebuild_s3_policy" {
-  name        = "pigeon-pictures-codebuild-s3-policy"
-  description = "Allow CodeBuild to access and write to the pigeon.pictures bucket only"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid : "AllowS3AccessToPigeonPictures",
-        Effect : "Allow",
-        Action : [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:ListBucket"
-        ],
-        Resource : [
-          "arn:aws:s3:::pigeon.pictures",
-          "arn:aws:s3:::pigeon.pictures/*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "pigeon_codebuild_s3_policy_attach" {
-  role       = aws_iam_role.pigeon_codebuild_role.name
-  policy_arn = aws_iam_policy.pigeon_codebuild_s3_policy.arn
-}
-
-# Policy for CloudWatch Logs permissions
-resource "aws_iam_policy" "pigeon_codebuild_cloudwatch_policy" {
-  name        = "pigeon-pictures-codebuild-cloudwatch-policy"
-  description = "Allow CodeBuild to create and write CloudWatch Logs for the pigeon.pictures project"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid : "AllowCloudWatchLogs",
-        Effect : "Allow",
-        Action : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource : "arn:aws:logs:eu-central-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/codebuild/pigeon-pictures-codebuild-project*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "pigeon_codebuild_cloudwatch_policy_attach" {
-  role       = aws_iam_role.pigeon_codebuild_role.name
-  policy_arn = aws_iam_policy.pigeon_codebuild_cloudwatch_policy.arn
-}
-
-resource "aws_iam_policy" "pigeon_codebuild_secrets_policy" {
-  name        = "pigeon-pictures-codebuild-secrets-policy"
-  description = "Allow CodeBuild to retrieve secret values from Secrets Manager for PIXABAY_API_KEY"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid : "AllowGetSecretValue",
-        Effect : "Allow",
-        Action : [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret"
-        ],
-        Resource : aws_secretsmanager_secret.pixabay_api_key.arn
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "pigeon_codebuild_secrets_policy_attach" {
-  role       = aws_iam_role.pigeon_codebuild_role.name
-  policy_arn = aws_iam_policy.pigeon_codebuild_secrets_policy.arn
-}
-
-#############################
-# CodeBuild Project for pigeon.pictures Website
-#############################
-resource "aws_codebuild_project" "pigeon_codebuild_project" {
-  name          = "pigeon-pictures-codebuild-project"
-  description   = "CodeBuild project for building and deploying the pigeon.pictures website, triggered by EventBridge schedule"
-  build_timeout = 60
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-
-    # Inject the PIXABAY_API_KEY from Secrets Manager
-    environment_variable {
-      name  = "PIXABAY_API_KEY"
-      value = aws_secretsmanager_secret.pixabay_api_key.arn
-      type  = "SECRETS_MANAGER"
-    }
-  }
-
-  service_role = aws_iam_role.pigeon_codebuild_role.arn
-
-  source {
-    type      = "GITHUB"
-    location  = "https://github.com/amireldor/pigeon.pictures"
-    buildspec = "buildspec.yml"
-  }
-
-  artifacts {
-    type                   = "S3"
-    location               = "pigeon.pictures"
-    packaging              = "NONE"
-    path                   = ""
-    name                   = "astro-generated-website"
-    override_artifact_name = true
-    namespace_type         = "NONE"
-    encryption_disabled    = true
-  }
-}
-
-#########################################
-# IAM Role for EventBridge (pigeon.pictures)
-#########################################
-resource "aws_iam_role" "pigeon_eventbridge_role" {
-  name = "pigeon-pictures-eventbridge-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
-        Principal = { Service = "events.amazonaws.com" }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "pigeon_eventbridge_policy" {
-  name = "pigeon-pictures-eventbridge-policy"
-  role = aws_iam_role.pigeon_eventbridge_role.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action   = "codebuild:StartBuild",
-        Effect   = "Allow",
-        Resource = aws_codebuild_project.pigeon_codebuild_project.arn
-      }
-    ]
-  })
-}
-
-#########################################
-# EventBridge Rule to Schedule the Build for pigeon.pictures
-#########################################
-resource "aws_cloudwatch_event_rule" "pigeon_codebuild_schedule" {
-  name                = "pigeon-pictures-codebuild-schedule"
-  description         = "Triggers the pigeon.pictures CodeBuild project every 60 minutes"
-  schedule_expression = "cron(0/60 * * * ? *)"
-}
-
-#########################################
-# EventBridge Target: Connect the Rule to the CodeBuild Project for pigeon.pictures
-#########################################
-resource "aws_cloudwatch_event_target" "pigeon_codebuild_target" {
-  rule      = aws_cloudwatch_event_rule.pigeon_codebuild_schedule.name
-  target_id = "PigeonPicturesCodeBuildTarget"
-  arn       = aws_codebuild_project.pigeon_codebuild_project.arn
-  role_arn  = aws_iam_role.pigeon_eventbridge_role.arn
-}
-
-# CloudFront distribution to serve from S3 bucket at /pigeons-2025
+# -----------------------------------------------------------------------------
+# CLOUDFRONT DISTRIBUTION
+# -----------------------------------------------------------------------------
 resource "aws_cloudfront_distribution" "pigeon_pictures_cdn" {
   origin {
     domain_name = aws_s3_bucket_website_configuration.pigeon_pictures_website.website_endpoint
